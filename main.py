8618e62f819c0dffebacb2cca3a34d7041c67fbd
nboulfrad <7568632+nboulfrad@users.noreply.github.com>
2020-06-25 14:46:29 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import time
-import schedule
-import hashtag_search
-
-if __name__ == "__main__":
-    schedule.every(3).hours.do(hashtag_search.hasht)
-    while True:
-        schedule.run_pending ()
-        time.sleep ( 1 )
